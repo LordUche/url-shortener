b7c8cc48f7c787abb49994e8a9e74e6d2474809f
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2020-09-27 23:28:27 +0100

    :tada: did it

---
+++ index.js
@@ -0,0 +1,93 @@
+require('dotenv/config')
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const compression = require('compression')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const mongoSanitize = require('mongo-sanitize')
+const yup = require('yup')
+const { nanoid } = require('nanoid')
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('✨ Database connection successful!'))
+  .catch((error) => console.log('💥', error.message))
+
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[a-zA-Z0-9_-]/)
+    .lowercase()
+    .nullable(),
+  url: yup.string().url().required()
+})
+
+const linkSchema = new mongoose.Schema(
+  {
+    slug: {
+      type: String,
+      unique: [true, 'Slug is taken'],
+      trim: true,
+      required: [true, 'Slug is required']
+    },
+    url: { type: String, required: [true, 'URL is required'] }
+  },
+  { toObject: { versionKey: false }, toJSON: { versionKey: false } }
+)
+
+const Link = mongoose.model('Link', linkSchema)
+
+const app = express()
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(express.json())
+app.use(mongoSanitize())
+if (app.get('env') === 'development') app.use(morgan('tiny'))
+
+app.get('/:slug', async (req, res, next) => {
+  try {
+    const { slug } = req.params
+    const link = await Link.findOne({ slug })
+    if (!link) {
+      const error = new Error('Link not found')
+      error.status = 404
+      next(error)
+    }
+    res.redirect(link.url)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.post('/url', async (req, res, next) => {
+  try {
+    let { slug, url } = req.body
+    if (!slug) slug = nanoid(5)
+    const validBody = await schema.validate({ slug, url }, { stripUnknown: true })
+    const link = await Link.create(validBody)
+    res.status(201).json(link)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use((error, req, res, next) => {
+  error.status ? res.status(error.status) : res.status(500)
+  if (error.name === 'MongoError' && error.code === 11000) {
+    error.message = 'Slug is taken'
+  }
+  res.json({
+    message: error.message,
+    stack: app.get('env') === 'development' ? error.stack : undefined
+  })
+})
+
+const PORT = process.env.PORT || 1337
+app.listen(PORT, () => console.log('🚀 App started on port', PORT))
